36e8f342741be68a341f24faf924dec35d9cd416
Simba Blessed <simbamahla@gmail.com>
2023-03-23 00:19:24 +0200

    first commit

---
+++ assets/js/mine.js
@@ -0,0 +1,23 @@
+/* $(function(){
+
+    $(".card").hover(function(){
+        $("#card-a1").animate({
+            top:30,
+        })
+    
+        $("#card-a1").animate({
+            bottom:0,
+        })
+    })
+    
+    }) */
+
+
+/* $(function(){
+
+    $(".card").hover(function(){
+        $(".card").slideToggle(1000);
+    
+    })
+}) */
+
